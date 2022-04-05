7db384d887ea6967e6ab42cf2a3901aab61b0f9f
SamGG <samuel.granjeaud@inserm.fr>
2022-04-05 23:55:13 +0200

    Pheno functions updated and renamed; add a function to check consistency of pheno

--- R/fb_pheno.R
+++ R/fb_pheno.R
@@ -1,4 +1,4 @@
-#' @title fb_open_pheno
+#' @title fb_read_pheno
 #'
 #' @description .
 #'
@@ -6,10 +6,10 @@
 #' @param file string
 #'
 #' @importFrom readxl read_xlsx
-#' @importFrom checkmate assertClass assertString assert testClass
+#' @importFrom checkmate assertClass assertString assertFileExists testClass
 #' @export
 
-fb_open_pheno <- function(
+fb_read_pheno <- function(
   fb,
   file
 ) {
@@ -17,14 +17,14 @@ fb_open_pheno <- function(
   if (missing(file))
     file <- fb_file_name(fb, "%s-pheno.xlsx")
   assertString(file)
-  assert(file.exists(file))
+  assertFileExists(file)
   if (grepl("xlsx$", file)) {
     pheno <- readxl::read_excel(file)
     pheno <- as.data.frame(pheno)
   } else
     stop("Unrecognized file format for pheno.")
   if (testClass(pheno, "data.frame")) {
-    # do some basic checks
+    # TODO: check that it is a pheno file
   }
   fb@pheno <- pheno
   fb
@@ -39,19 +39,76 @@ fb_open_pheno <- function(
 #' @param fb flowBunch
 #'
 #' @importFrom writexl write_xlsx
-#' @importFrom checkmate assertClass
+#' @importFrom checkmate assertClass assertPathForOutput
 #' @export
 
 fb_write_pheno <- function(
-  fb
+  fb,
+  mini = FALSE
 ) {
   assertClass(fb, "flowBunch")
   file <- fb_file_name(fb, "%s-pheno.xlsx")
-  assert(dir.exists(dirname(file)))
+  assertPathForOutput(file, overwrite = TRUE)
   if (grepl("xlsx$", file)) {
     if (file.exists(file))
       file.rename(file, timetag(file))
-    writexl::write_xlsx(fb@pheno, file)
+    # minimalize pheno
+    pheno <- fb@pheno
+    if (mini) {
+      # simplify file_name
+      pheno$file_name <- unname(sapply(pheno$file_name, basename))
+    }
+    # write pheno
+    writexl::write_xlsx(pheno, file)
   } else
     stop("Unrecognized file format for pheno. No data written to disk.")
+}
+
+
+
+#' @title fb_check_pheno
+#'
+#' @description .
+#'
+#' @param fb flowBunch
+#'
+#' @importFrom checkmate assertClass
+#' @export
+
+fb_check_pheno <- function(
+  fb
+) {
+  assertClass(fb, "flowBunch")
+  pheno <- fb@pheno
+
+  errors <- list()
+  # identify FCS with no batch_id
+  ko <- is.na(pheno$batch_id) | trimws(pheno$batch_id) == ""
+  if (any(ko)) errors <- c(errors, fcs_without_batch = sprintf(
+    "FCS do not have batch_id: %s",
+    paste0(pheno$sample_id[ko], collapse = ", ")))
+  # extract anchor per batch
+  tbl <- table(pheno$sample_is_ref, pheno$batch_id)
+  # identify batch with 0 anchor
+  ko <- tbl["Y",] == 0
+  if (any(ko)) errors <- c(errors, batch_without_anchor = sprintf(
+    "Batches do not have an anchor: %s",
+    paste0(colnames(tbl)[ko], collapse = ", ")))
+  # identify batch with >1 anchors
+  ko <- tbl["Y",] > 1
+  if (any(ko)) errors <- c(errors, batch_with_many_anchors = sprintf(
+    "Batches have more than 1 anchor: %s",
+    paste0(colnames(tbl)[ko], collapse = ", ")))
+  # extract reference
+  tbl <- table(pheno$batch_is_ref, pheno$batch_id)
+  # reference is an anchor
+  ko <- sum(tbl["Y",]) == 0
+  if (any(ko)) errors <- c(errors, bunch_without_ref =
+                             "No batch is reference")
+  # reference is unique
+  ko <- sum(tbl["Y",]) > 1
+  if (any(ko)) errors <- c(errors, bunch_with_many_refs =
+                             "More than 1 batch is reference")
+  # done
+  errors
 }
