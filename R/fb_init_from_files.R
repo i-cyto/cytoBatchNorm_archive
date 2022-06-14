0164c3635dc3d8010a97607ed98523e724f9248b
SamGG <samuel.granjeaud@inserm.fr>
2022-06-14 09:39:44 +0200

    fb_init_from_files now relies on fb_scan_files.

--- R/fb_init_from_files.R
+++ R/fb_init_from_files.R
@@ -1,16 +1,18 @@
 #' @title fb_init_from_files
 #'
-#' @description Mainly based on read.flowSet function.
+#' @description Initialize a flowBunch from a set of files, checking their
+#'   parameters and channels. Based on fb_scan_files function, similar to
+#'   read.flowSet function.
 #'
 #' @param fb flowBunch
-#' @param files strings, the vector of file names. NULL by default, which leads to load all FCS files in the given path.
+#' @param files strings, the vector of file names. NULL by default, which leads
+#'   to load all FCS files in the given path.
 #' @param path string, the unique path where the files are located.
 #' @param pattern string.
-#' @param which.lines integer, the event to load for the quick scan.
+#' @param which.lines integer, the events to load for the quick scan.
 #' @param channel_alias strings, see read.flowSet
 #' @param ... parameters passed to read.flowSet
 #' @param verbose integer, level of verbosity
-#' @param with_date_time logical, extract the date and time from FCS header
 #'
 #' @importFrom stats quantile
 #' @importFrom checkmate assertClass assertLogical assertIntegerish
@@ -24,55 +26,54 @@ fb_init_from_files <- function(
   which.lines = 1:50,
   channel_alias = NULL,
   ...,
-  verbose = 0,
-  with_date_time = FALSE
+  verbose = 0
 ) {
   assertClass(fb, "flowBunch")
-  assertLogical(with_date_time, len = 1)
   assertIntegerish(verbose, lower = 0, len = 1)
+  # TODO: manage options overlap with ...
   # clear expressions
   fb@exprs <- NULL
   # read all FCS files through read.flowSet which does consistency checks
   if (verbose) message("Scanning a bunch of FCS files...")
-  flowSet <- do.call(
-    "read.flowSet",
+  fscan <- do.call(
+    "fb_scan_files",
     c(
       files = files, path = path, pattern = pattern,
-      which.lines = which.lines,
-      channel_alias = channel_alias,
-      fb@options$read_fcs, # TODO: manage options overlap
+      outfile = NULL,
+      which.lines = which.lines, channel_alias = channel_alias,
+      fb@options$read_fcs,
       ...
     )
   )
+  # return
+  if (attr(fscan, "is_compliant") == FALSE) {
+    message("Initialization aborted: files are not homogeneous.")
+    return(NULL)
+  }
   if (verbose) message("Preparing panel and pheno")
   # create panel
-  info <- pData(parameters(flowSet[[1]]))
-  info[is.na(info$desc), "desc"] <- info[is.na(info$desc), "name"]
   panel <- data.frame(
-    fcs_colname = info$name,
-    antigen = info$desc,
-    guess_antigen = guess_antigen(info$desc),
+    fcs_colname = as.character(fscan[1,grep("^P\\d+N", colnames(fscan))]),
+    antigen = as.character(fscan[1,grep("^P\\d+S", colnames(fscan))]),
+    guess_antigen = NA,
     comment = ""
   )
+  idx <- is.na(panel$antigen)
+  panel[idx, "antigen"] <- panel[idx, "fcs_colname"]
+  panel$guess_antigen <- guess_antigen(panel$antigen)
   fb@panel <- panel
-  # create metadata
-  pheno <- fsApply(flowSet, function(ff) {
-    c(file_name = keyword(ff, "FILENAME")[[1]],
-      sample_id = identifier(ff),
-      TOT = keyword(ff, "$TOT")[[1]],
-      PAR = keyword(ff, "$PAR")[[1]],
-      FIL = keyword(ff, "$FIL")[[1]],
-      if (with_date_time) c(
-        DATE = keyword(ff, "$DATE")[[1]],
-        BTIM = keyword(ff, "$BTIM")[[1]],
-        ETIM = keyword(ff, "$ETIM")[[1]])
-    )
-  })
-  pheno <- data.frame(pheno, stringsAsFactors = FALSE)
+  # create pheno/metadata
+  pheno <- data.frame(
+    file_name = as.character(fscan[, "file_fullname"]),
+    sample_id = NA,
+    fscan[, c("TOT", "PAR", "DATE", "BTIM", "ETIM")],
+    stringsAsFactors = FALSE
+  )
+  sample_id <- basename(pheno$file_name)
+  sample_id <- gsub("\\.fcs$", "", sample_id, ignore.case = TRUE)
+  pheno$sample_id <- sample_id
   pheno$TOT <- as.integer(pheno$TOT)
-  if (!with_date_time) {
-    pheno
-  }
+  pheno$PAR <- as.integer(pheno$PAR)
   fb@pheno <- pheno
   # store input
   fb@input$files <- files
